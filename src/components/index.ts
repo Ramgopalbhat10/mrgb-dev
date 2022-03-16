7c3188ef0fb8f9b0cdfe84fadad715685d360943
Subbu <ramgopalbhat10@gmail.com>
2022-03-16 17:06:20 +0530

    added backround image and blog page

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export { default as Landing } from "./Landing.astro";
 export { default as Head } from "./common/Head.astro";
 export { default as Header } from "./common/Header.astro";
 export { default as Footer } from "./common/Footer.astro";
+export { default as Blog } from "./blog/Blog.astro";
